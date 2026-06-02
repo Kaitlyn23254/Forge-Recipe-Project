2f34e6f93f7a091dde91d18c66691d9743cbaa75
kuchimpos <kuchimpos@g.ucla.edu>
2026-06-02 13:14:02 -0700

    Create Comment component and styles

---
+++ frontend/src/components/Comment.jsx
@@ -0,0 +1,22 @@
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+
+import "./styles/Comment.css";
+
+export default function Comment({ username, text, createdAt, numLikes }) {
+  return (
+    <div className="comment">
+      <div className="comment-header">
+        <p className="comment-username">{username}</p>
+        <p>{createdAt}</p>
+      </div>
+      <div className="comment-body">
+        <p>{text}</p>
+      </div>
+      <div className="comment-footer">
+        <ThumbUpOffAltIcon />
+        <span>{numLikes}</span>
+      </div>
+      <p></p>
+    </div>
+  );
+}
