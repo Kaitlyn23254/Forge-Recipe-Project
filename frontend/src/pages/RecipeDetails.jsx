1211e4df22f5fcef6dfca802276ed2327442e601
kuchimpos <kuchimpos@g.ucla.edu>
2026-06-02 06:51:21 -0700

    Create IngredientsList component
    Created IngredientsList.jsx to use in RecipeDetails. Installed MaterialUI. Added IngredidentsList to RecipeDetails page with mock data

--- frontend/src/pages/RecipeDetails.jsx
+++ frontend/src/pages/RecipeDetails.jsx
@@ -1,3 +1,12 @@
+import IngredientsList from "../components/IngredientsList";
+
+const mockIngredients = [
+  { ingredient: "carrot", measurement: "3.4 cup" },
+  { ingredient: "onion", measurement: "1 large" },
+  { ingredient: "garlic", measurement: "2 cloves" },
+  { ingredient: "olive oil", measurement: "2 tbsp" },
+];
+
 export default function RecipeDetails() {
-  return <p>This is the recipe details page</p>;
+  return <IngredientsList ingredients={mockIngredients} />;
 }
